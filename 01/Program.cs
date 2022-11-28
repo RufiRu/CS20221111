bed30f569ff68bdc6c69378d68e80c4991070140
Тимошкина Руфия <rufchik85@mail.ru>
2022-11-29 00:06:14 +0300

    Lesson 1

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. С клавиатуры вводится целое число. Вывести квадрат числа (блок-схема)
+int a;
+a=10;
+string? s=Console.ReadLine ();
+a=Convert.ToInt32 (s);
+int b=a*a;
+System.Console.WriteLine (b);
+
+
+
+
+
+
+
+
