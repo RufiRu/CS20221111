d7e0d7fb80b4c32657438a175eff18a5c9d7f6a3
Тимошкина Руфия <rufchik85@mail.ru>
2022-11-30 01:02:49 +0300

    HW2

---
+++ BasicAlgorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿// Обмен значениями двух переменных
+
+//int a=5;
+//int b=7;
+//int temp;
+//temp=a;
+//a=b;
+//b=temp;
+
+
+int a=5;
+int b=7; 
+a=a^b;
+b=a^b;
+a=a^b;
+System.Console.WriteLine($"a={a} b={b}");
---
+++ СчетчикФлаг/Program.cs
@@ -0,0 +1,12 @@
+﻿// Счетчик. Флаг
+int n=int.Parse(Console.ReadLine());
+int k=0;//счетчик
+bool flag=false;//флаг
+for(int i=0;i<n;i++)
+{
+    int a=int.Parse(Console.ReadLine());
+    if (a<5) k++;
+    if (a==10) flag=true;
+}
+System.Console.WriteLine(k);
+if (flag) System.Console.WriteLine("YES");else System.Console.WriteLine("NO");
